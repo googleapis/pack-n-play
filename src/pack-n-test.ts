600b5a473ddb35e753ee7a877dcb6a9b57523c3f
Daniel Bankhead <danielbankhead@google.com>
2023-10-27 10:58:46 -0700

    fix: Use Target `es2018` to Match `gts`

--- src/pack-n-test.ts
+++ src/pack-n-test.ts
@@ -144,7 +144,7 @@ export async function packNTest(options: TestOptions) {
 
     if (sample.ts) {
       // TODO: maybe make it flexible for users to pass in typescript config.
-      await execa('npx', ['tsc', '--strict', 'index.ts'], {
+      await execa('npx', ['tsc', '--target', 'es2018', '--strict',  'index.ts'], {
         cwd: installDir,
       });
     }
