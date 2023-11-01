329ef808b688e8a08fe3ea69bc8e80d8ba2d9fac
Daniel Bankhead <danielbankhead@google.com>
2023-11-01 14:27:36 -0700

    fix: mocha config

--- .mocharc.js
+++ .mocharc.js
@@ -12,18 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 const config = {
-  "enable-source-maps": true,
-  "throw-deprecation": true,
-  "timeout": 10000,
-  "recursive": true
-}
+  'enable-source-maps': true,
+  'throw-deprecation': true,
+  timeout: 300000,
+  recursive: true,
+};
+
 if (process.env.MOCHA_THROW_DEPRECATION === 'false') {
   delete config['throw-deprecation'];
 }
+
 if (process.env.MOCHA_REPORTER) {
   config.reporter = process.env.MOCHA_REPORTER;
 }
+
 if (process.env.MOCHA_REPORTER_OUTPUT) {
   config['reporter-option'] = `output=${process.env.MOCHA_REPORTER_OUTPUT}`;
 }
-module.exports = config
+
+module.exports = config;
